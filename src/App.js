1d50885d23c9164417375f01229be05de79b5a0c
unknown <arianarangel97@gmail.com>
2020-07-27 18:01:00 -0600

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Home from './components/Home';
+import Home from './containers/Home';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import store from './redux/store';
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,13 @@
 import { connect } from 'react-redux';
 import Home from '../components/Home';
+import { getAllShelter } from '../redux/actions';
 
 // Import related actions
-
 
 const mapStateToProps = (state) => {
     // Will always return a JSON object with fields mapped to the fields defined in the Redux store.
     return {
-        
+        allShelter: state.allShelter,
         // NOTE we are now tying the seen property that will be used in the Home component to the Redux store seen field (via state.seen)
     };
 }
@@ -15,7 +15,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         // Contains the components' properties tied to the dispatched actions
-        
+        getAllShelter: (shelters) => dispatch(getAllShelter(shelters)),
+
     }
 }
 
